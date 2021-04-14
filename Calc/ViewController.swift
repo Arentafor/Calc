a9ca3ae9884a90344480527b22d9b98732000d66
Vitas <arentafor@gmail.com>
2021-04-14 18:19:36 +0300

    Pre-release

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -8,12 +8,118 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func EasyOprations(_ sender: UIButton) {
+        operacion = sender.currentTitle!
+        memory = current
+        print(operacion)
+        isSecond = false
+        doubeled = false
     }
-
-
+    
+    
+    
+    @IBOutlet weak var ResultLabel: UILabel!
+    var isFirstOperation = true
+    var isSecond = false
+    var doubeled = false
+    var memory: Double = 0
+    var second: Double = 0
+    var operacion: String = ""
+    var current:Double {
+        get {
+            return Double(ResultLabel.text!)!
+        
+        }
+        set {
+            let value = "\(newValue)"
+            ResultLabel.text = "\(newValue)"
+            let valueArray = value.components(separatedBy: ".")
+            if valueArray.count > 1 && valueArray[1] == "0" {
+                ResultLabel.text = valueArray[0]
+            } else {
+                ResultLabel.text = "\(newValue)"
+            }
+            isSecond = false
+        }
+    
+    }
+    @IBAction func PressKeyNumberBottom(_ sender: UIButton) {
+        let n = sender.currentTitle!
+        print(n)
+        if isSecond {
+            if ((ResultLabel.text?.count)! < 15) {
+            ResultLabel.text = ResultLabel.text! + n
+            }
+        }
+        else {
+            if ((ResultLabel.text?.count)! < 15) {
+            ResultLabel.text = n
+            isSecond = true
+            }
+        }
+        
+    }
+    
+    @IBAction func EqualPressed(_ sender: UIButton) {
+       if isSecond {
+            second = current
+        
+            if isFirstOperation == false {
+                switch operacion {
+                case "+":
+                    current = memory + second
+                    isSecond = false
+                case "-":
+                    current = memory - second
+                    isSecond = false
+                case "*":
+                    current = memory * second
+                    isSecond = false
+                case "/":
+                    current = memory / second
+                    isSecond = false
+                default:
+                    break
+                }
+            }
+       }
+      
+        isFirstOperation = false
+        doubeled = false
+    }
+    
+    
+    @IBAction func Clearbottom(_ sender: UIButton) {
+        doubeled = false
+        isSecond = false
+        memory = 0
+        second = 0
+        operacion = ""
+        current = 0
+        ResultLabel.text = "0"
+        isFirstOperation = true
+    }
+    @IBAction func PlusMinusbottom(_ sender: UIButton) {
+        current = -current
+        if ResultLabel.text == "-0" {
+            ResultLabel.text = "0"
+        }
+    }
+    
+    @IBAction func Percentbottom(_ sender: UIButton) {
+        current = current / 100
+    }
+    
+    @IBAction func Dotbottom(_ sender: UIButton) {
+        if isSecond && !doubeled {
+            ResultLabel.text = ResultLabel.text! + "."
+            doubeled = true
+        }
+        else if !isSecond && !doubeled {
+            ResultLabel.text = "0."
+        }
+    }
+  
 }
 
